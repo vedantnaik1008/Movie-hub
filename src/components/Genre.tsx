8941991ef2c12dcd31baf452dd592284c5dfbe42
tallman <vedunaik@gmail.com>
2023-12-13 12:01:56 +0530

    +genre hook

--- src/components/Genre.tsx
+++ src/components/Genre.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react'
-import { GenreData, ValueData } from '../Pages/Movies';
+import { useEffect } from 'react'
+import { ValueData } from '../Pages/Movies';
 import { APIKEY } from '../Services/api-client';
+import useGenre from '../hooks/useGenre';
 
 interface Props{
     type: string;
@@ -10,8 +11,7 @@ interface Props{
 
 
 const Genre = ({  type, value, setValue }: Props) => {
-  const [genre, setGenre] = useState<GenreData[]>([]);
-  const [page, setPage] = useState(1);
+  const {page,  genre, setGenre, CategoryAdd, CategoryRemove}  = useGenre({setValue, value});
   useEffect(() => {
     const fetchGenre = async () => {
       const data = await fetch(
@@ -20,25 +20,12 @@ const Genre = ({  type, value, setValue }: Props) => {
       const { genres } = await data.json();
       console.log(genres);
       setGenre(genres);
-      
-      
     };
     fetchGenre();
   }, [setGenre, type]);
   
   console.log(page);
-  const CategoryAdd = (genres: ValueData) => {
-    setValue([...value, genres]);
-    setGenre(genre.filter((g)=> g.id !== genres.id));
-    setPage(1)
-  }
 
-  const CategoryRemove = (genres: ValueData) => {
-    setValue(value.filter((g)=> g.id !== genres.id));
-    setGenre([...genre, genres]);
-    setPage(1)
-  }
-  
   return (
     <>
       <div className="genre-container">
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,14 +1,35 @@
+import { useState } from "react";
+import { GenreData } from "../Pages/Movies";
+
 export interface ValueData {
     id: number;
     name: string;
     genres: [];
   }
+
+  interface Props{
+    value: ValueData[];
+    setValue: (value: ValueData[]) => void;
+  }
   
-  const useGenre = (value: ValueData[]): string => {
-    if (value.length < 1) return '';
-    const genreIds = value.map((g) => g.id);
-    return genreIds.reduce((acc, curr) => acc + ',' + curr.toString(), '');
-  };
+  const useGenre = ({setValue, value}: Props) => {
+    const [genre, setGenre] = useState<GenreData[]>([]);
+    const [page, setPage] = useState(1);
+
+    const CategoryAdd = (genres: ValueData) => {
+      setValue([...value, genres]);
+      setGenre(genre.filter((g)=> g.id !== genres.id));
+      setPage(1)
+    }
+  
+    const CategoryRemove = (genres: ValueData) => {
+      setValue(value.filter((g)=> g.id !== genres.id));
+      setGenre([...genre, genres]);
+      setPage(1)
+    }
+
+    return {page, setPage, genre, setGenre, CategoryAdd, CategoryRemove};
+};
   
   export default useGenre;
   
